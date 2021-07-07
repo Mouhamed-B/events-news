1a26130c5c3cac05ae0fa339146916e134c5125a
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2021-07-07 15:54:15 +0000

    renamed navbar to event-list

---
+++ src/app/event-list/event-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-list',
+  templateUrl: './event-list.component.html',
+  styleUrls: ['./event-list.component.scss']
+})
+export class EventList implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
