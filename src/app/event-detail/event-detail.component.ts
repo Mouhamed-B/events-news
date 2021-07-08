4342e5e470c09cad248a52d972133e0b46aa5827
Mouhamed Bakhoum <bakhoumouhamed@gmail.com>
2021-07-08 10:44:53 +0000

    feat:completed event-detail component

--- src/app/event-detail/event-detail.component.ts
+++ src/app/event-detail/event-detail.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-event-detail',
+  selector: 'event-detail',
   templateUrl: './event-detail.component.html',
   styleUrls: ['./event-detail.component.scss']
 })
 export class EventDetailComponent implements OnInit {
 
+  @Input() event : any;
   constructor() { }
 
   ngOnInit(): void {
